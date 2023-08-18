67178bcd3b7ebb8440fe224790e1b806e3ec388d
gryatka <edameow1@gmail.com>
2023-08-18 22:44:20 +0500

    init work on test data

---
+++ src/Controller/ListController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/task-list', name: 'app_list')]
+class ListController extends AbstractController
+{
+    #[Route('/', name: 'app_list')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ListController.php',
+        ]);
+    }
+}
